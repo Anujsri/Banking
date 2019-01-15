b2c9ce8adfea9205c24cd995e5b016fe839ed051
Anuj Srivastava <anujsrivastava@iiitdmj.ac.in>
2019-01-15 13:30:00 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports={
     mongoURI:'your db',
-    googleClientID:'26795804891-6rpn9mm2qfrflanmdbhaomojmtdpclvo.apps.googleusercontent.com',
-    googleClientSecret:'-fS3dJ2FXxYqimKsWv-9GwD-',
-    facebookClientID:'244957112848689',
-    facebookClientSecret:'fb32f90269d0c89dd2279252454208cc'
+    googleClientID:'**************',
+    googleClientSecret:'****************',
+    facebookClientID:'*****************',
+    facebookClientSecret:'*****************'
 }
